7d259637e5ba5f40f887d81b68ab8df5e4550243
Atharv <atharvpjaju@gmail.com>
2021-10-26 22:08:20 +0530

    Search Model created 1. Created Search(class) in model(.py) 2. Registered model in admin.py 3. Run commands : makemigrations and migrate 4. stop and runserver command
    Note: Superuser was created

--- Craigslist_Clone/Craigslist_Clone_app/models.py
+++ Craigslist_Clone/Craigslist_Clone_app/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
+class Search(models.Model):
+    search = models.CharField(max_length=500)
+    created = models.DateTimeField(auto_now=True)
