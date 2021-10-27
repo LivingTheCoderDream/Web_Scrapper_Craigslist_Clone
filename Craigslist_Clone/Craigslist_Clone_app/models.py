ecb619691a3cfe8166e4551da25ea69bf08f373a
Atharv <atharvpjaju@gmail.com>
2021-10-27 18:55:47 +0530

    Searches Model for Admin Created
    1. Search model added to models.py
     a. __str__ to retrun search name not object 1
     b. Meta to cahnge plural from Searchs to Searches
    
    2. Run commands makemigrations and migrate

--- Craigslist_Clone/Craigslist_Clone_app/models.py
+++ Craigslist_Clone/Craigslist_Clone_app/models.py
@@ -6,3 +6,11 @@ from django.db import models
 class Search(models.Model):
     search = models.CharField(max_length=500)
     created = models.DateTimeField(auto_now=True)
+
+    # We don't want to see the newly added object as Search object 1, Rather we want the search itself as the name
+    def __str__(self):
+        return '{}'.format(self.search)
+
+    # We don't want to see Searchs as it is wrong so changing the plural to Searches
+    class Meta:
+        verbose_name_plural = 'Searches'
