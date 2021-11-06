aff284742f5d28b70afee95f9ad43acc65eea579
Atharv <atharvpjaju@gmail.com>
2021-11-06 09:08:55 +0530

    base.html almost finalized + new_search.html started
    1. Added materialize front-end to the base.html so it renders the NAV-BAR and ICONS.
    
    2. This NAV BAR and ICONS will stay on top of each url in our project (eg. new_search) so we will use DRY principle.
    
    3. Created directory my_app under the already present templates and added new_search.html in it which is currently blank.

--- Craigslist_Clone/Craigslist_Clone_app/views.py
+++ Craigslist_Clone/Craigslist_Clone_app/views.py
@@ -6,3 +6,6 @@ from django.shortcuts import render
 def home(request):
     return render(request, template_name='base.html')
 
+
+def new_search(request):
+    return render(request, template_name='my_app/new_search.html')
